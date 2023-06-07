6d1bed826c46858e212784381a09540d405b5532
IBST-yoonSangHyeon <dalpework@gmail.com>
2023-06-07 22:47:02 +0900

    움직이는 배경 만들기

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float moveSpeed = 3f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Time.deltaTime 컴퓨터 사양의 프레임에 따라 이동되는 것을 같도록 함.
+
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime; 
+        if (transform.position.y <= -10f){
+            transform.position += new Vector3(0f, 20f, 0f);
+        }
     }
 }
