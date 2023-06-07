62d9b2f3ca624c2dea402726d100d29700cef0ff
IBST-yoonSangHyeon <dalpework@gmail.com>
2023-06-07 23:49:42 +0900

    키보드로 움직이기 - 후반전

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,17 @@ public class Player : MonoBehaviour
     void Update()
     {
         // 방법 1
-        float horizontalInput = Input.GetAxisRaw("Horizontal"); // 키보드 방향 좌우
-        //float verticalInput = Input.GetAxisRaw("Vertical"); // 키보드 뱡향 위아래
-        Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
-        transform.position += moveTo * moveSpeed * Time.deltaTime;
+        // float horizontalInput = Input.GetAxisRaw("Horizontal"); // 키보드 방향 좌우
+        // //float verticalInput = Input.GetAxisRaw("Vertical"); // 키보드 뱡향 위아래
+        // Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
+        // transform.position += moveTo * moveSpeed * Time.deltaTime;
 
-
+        // 방법 2
+        Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position -= moveTo;
+        } else if ( Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += moveTo;
+        }
     }
 }
