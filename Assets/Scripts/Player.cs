429734183a97f6d6f790f0b6c75961a3a3d26ca5
IBST-yoonSangHyeon <dalpework@gmail.com>
2023-06-08 23:01:17 +0900

    미사일 만들기 - 후반전

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,12 @@ public class Player : MonoBehaviour
 {   
     [SerializeField]
     private float moveSpeed;
+
+    [SerializeField]
+    private GameObject weapon; // 무기 객체 변수
+
+    [SerializeField]
+    private Transform shootTransform; // shootTransform 객체 위치 변수 
     // Update is called once per frame
     void Update()
     {
@@ -27,5 +33,12 @@ public class Player : MonoBehaviour
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float toX = Mathf.Clamp(mousePos.x , -2.35f, 2.35f);
         transform.position = new Vector3(toX, transform.position.y, transform.position.z);
+
+        // 무기 발사
+        Shoot();
+    }
+
+    void Shoot() {
+        Instantiate(weapon, shootTransform.position, Quaternion.identity);
     }
 }
